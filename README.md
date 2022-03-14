9fb58f6bf5879d052c4f6aa8ba148f1845a08ee1
Nick Sciarrilli <nsci@amazon.com>
2022-03-13 17:51:54 -1000

    init

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# bdd100k-instance-segmentation
+# BDD100k Instance Segmentation with Mask R-CNN
+
+This is a final project for a masters in data science at the University of Denver. Instance Segmentation with Berkeley DeepDrive dataset using [Mask R-CNN](https://arxiv.org/abs/1703.06870) on Python 3, Keras, and TensorFlow. The model generates bounding boxes and segmentation masks for each instance of an object in the image. It's based on Feature Pyramid Network (FPN) and a ResNet50 backbone.
+
+![Instance Segmentation Sample](assets/street.png)
+
+# Getting Started
+* [eda.ipynb](samples/bdd100k/1._Exploratory_Data_Analysis.ipynb) shows some statistical analysis on the bdd100k ins seg dataset. 
+
+* [data_inspect.ipynb](samples/bdd100k/2._Data_Inspection.ipynb) shows inspection of the BDD100k dataset and Mask R-CNN model. 
+
+* [model_training.ipynb](samples/bd100k/3._Model_Training.ipynb) It shows the Berkeley DeepDrive dataset trained for instance segmentation with Mask R-CNN
+
+* [data_inspect.ipynb](samples/bdd100k/4._Model_Evaluation.ipynb) shows evaluation of the Mask R-CNN model on the BDD100k dataset.
+
+## Requirements
+Python 3.4, TensorFlow 1.3, Keras 2.0.8 and other common packages listed in `requirements.txt`.
+
+
+
+
+
+
+
+
+
---
+++ samples/bdd100k/.ipynb_checkpoints/bdd100k-checkpoint.py
@@ -0,0 +1,532 @@
+"""
+Mask R-CNN
+Train on the bdd100k segmentation dataset from https://www.bdd100k.com/
+
+Written by Nick Sciarrilli
+
+------------------------------------------------------------
+
+Usage: import the module (see Jupyter notebooks for examples), or run from
+       the command line as such:
+
+    # Train a new model starting from ImageNet weights
+    python3 bdd100k.py train --dataset=/path/to/dataset --subset=train --weights=imagenet
+
+    # Train a new model starting from specific weights file
+    python3 bdd100k.py train --dataset=/path/to/dataset --subset=train --weights=/path/to/weights.h5
+
+    # Resume training a model that you had trained earlier
+    python3 bdd100k.py train --dataset=/path/to/dataset --subset=train --weights=last
+
+    # Generate submission file
+    python3 bdd100k.py detect --dataset=/path/to/dataset --subset=train --weights=<last or /path/to/weights.h5>
+"""
+
+# Set matplotlib backend
+# This has to be done before other importa that might
+# set it, but only if we're running in script mode
+# rather than being imported.
+if __name__ == '__main__':
+    import matplotlib
+    # Agg backend runs without a display
+    matplotlib.use('Agg')
+    import matplotlib.pyplot as plt
+
+import os
+import sys
+import json
+from tqdm import tqdm
+from pycocotools import mask
+import cv2
+import datetime
+import numpy as np
+import skimage.io
+from imgaug import augmenters as iaa
+from PIL import Image, ImageDraw
+
+# Root directory of the project
+ROOT_DIR = os.path.abspath("../../")
+
+# Import Mask RCNN
+sys.path.append(ROOT_DIR)  # To find local version of the library
+from mrcnn.config import Config
+from mrcnn import utils
+from mrcnn import model as modellib
+from mrcnn import visualize
+
+# Path to trained weights file
+COCO_WEIGHTS_PATH = os.path.join(ROOT_DIR, "mask_rcnn_coco.h5")
+
+# Directory to save logs and model checkpoints, if not provided
+# through the command line argument --logs
+DEFAULT_LOGS_DIR = os.path.join(ROOT_DIR, "logs/bdd100k")
+
+# Results directory
+# Save submission files here
+RESULTS_DIR = os.path.join(ROOT_DIR, "results/bdd100k/")
+
+
+
+############################################################
+#  Configurations
+############################################################
+
+
+class Bdd100kConfig(Config):
+    """Configuration for training on the bdd100k segmentation dataset."""
+    # Give the configuration a recognizable name
+    NAME = "bdd100k"
+
+    # Adjust depending on your GPU memory
+    IMAGES_PER_GPU = 4
+
+    # Number of classes (including background)
+    NUM_CLASSES = 1 + 8  # Background + pedestrian, rider, car, truck, bus, train, motorcycle, bicycle
+
+    # Number of training and validation steps per epoch
+    STEPS_PER_EPOCH = 100
+    VALIDATION_STEPS = 30
+
+    # Don't exclude based on confidence. Since we have two classes
+    # then 0.5 is the minimum anyway as it picks between nucleus and BG
+    # DETECTION_MIN_CONFIDENCE = 0
+
+    # Backbone network architecture
+    # Supported values are: resnet50, resnet101
+    BACKBONE = "resnet50"
+
+    # Input image resizing
+    # Random crops of size 512x512
+    #IMAGE_RESIZE_MODE = "pad64"
+    IMAGE_MIN_DIM = 720
+    IMAGE_MAX_DIM = 1280
+    # IMAGE_MIN_SCALE = 2.0
+
+    # Length of square anchor side in pixels
+    RPN_ANCHOR_SCALES = (8, 16, 32, 64, 128)
+
+    # ROIs kept after non-maximum supression (training and inference)
+    POST_NMS_ROIS_TRAINING = 1000
+    POST_NMS_ROIS_INFERENCE = 2000
+
+    # Non-max suppression threshold to filter RPN proposals.
+    # You can increase this during training to generate more propsals.
+    RPN_NMS_THRESHOLD = 0.9
+
+    # How many anchors per image to use for RPN training
+    RPN_TRAIN_ANCHORS_PER_IMAGE = 64
+
+    # Image mean (RGB)
+    MEAN_PIXEL = np.array([43.53, 39.56, 48.22])
+
+    # If enabled, resizes instance masks to a smaller size to reduce
+    # memory load. Recommended when using high-resolution images.
+    USE_MINI_MASK = True
+    MINI_MASK_SHAPE = (56, 56)  # (height, width) of the mini-mask
+
+    # Number of ROIs per image to feed to classifier/mask heads
+    # The Mask RCNN paper uses 512 but often the RPN doesn't generate
+    # enough positive proposals to fill this and keep a positive:negative
+    # ratio of 1:3. You can increase the number of proposals by adjusting
+    # the RPN NMS threshold.
+    TRAIN_ROIS_PER_IMAGE = 128
+
+    # Maximum number of ground truth instances to use in one image
+    MAX_GT_INSTANCES = 200
+
+    # Max number of final detections per image
+    DETECTION_MAX_INSTANCES = 400
+    
+    # The magical learning rate
+    #LEARNING_RATE = 0.01
+    
+config = Bdd100kConfig()
+config.display()
+
+
+class Bdd100kInferenceConfig(Bdd100kConfig):
+    # Set batch size to 1 to run one image at a time
+    GPU_COUNT = 1
+    IMAGES_PER_GPU = 1
+    # Non-max suppression threshold to filter RPN proposals.
+    # You can increase this during training to generate more propsals.
+    RPN_NMS_THRESHOLD = 0.65
+
+
+############################################################
+#  Dataset
+############################################################
+
+class Bdd100kDataset(utils.Dataset):
+
+    def load_bdd100k(self, annotation_json, images_dir):
+        """Load a subset of the bdd100k dataset.
+
+        dataset_dir: Root directory of the dataset
+        subset: Subset to load. Either the name of the sub-directory,
+                such as stage1_train, stage1_test, ...etc. or, one of:
+                * train: stage1_train excluding validation images
+                * val: validation images from VAL_IMAGE_IDS
+        """
+        
+        data = json.load(open(annotation_json))
+        
+        source_name = "bdd100k"
+        for category in data['categories']:
+            class_id = category['id']
+            class_name = category['name']
+            if class_id < 1:
+                print('Error: Class id for "{}" cannot be less than one. (0 is reserved for the background)'.format(class_name))
+                return
+            
+            self.add_class(source_name, class_id, class_name)
+
+
+        # Add images
+        annotations = {}
+        for annotation in tqdm(data['annotations']):
+            
+            # Initialize variables
+            image_id = annotation['image_id']
+            segmentation = []
+
+            # Decode the binary mask
+            mask_list = mask.decode(annotation['segmentation'])
+            mask_list = np.ascontiguousarray(mask_list, dtype=np.uint8)
+            contours, hierarchy = cv2.findContours((mask_list).astype(np.uint8), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+            # Get the contours
+            for contour in contours:
+                contour = contour.flatten().tolist()
+                segmentation.append(contour)
+                if len(contour) > 4:
+                    segmentation.append(contour)
+            if len(segmentation) == 0:
+                print('segmentation = 0')
+
+            if image_id not in annotations:
+                annotations[image_id] = []
+            annotation['segmentation'] = segmentation
+            annotation['iscrowd'] = 0
+            annotations[image_id].append(annotation)
+        
+#         out_file = open("./annotations_val.json", "w")
+#         json.dump(annotations, out_file, indent = 4)
+#         out_file.close()
+        
+#         if split == 'train':
+#             file = open('./annotations_train.json')
+#             annotations = json.load(file)
+#         elif split == 'val':
+#             file = open('./annotations_val.json')
+#             annotations = json.load(file)           
+
+        #seen_images = {}
+        images = []
+        for image in tqdm(data['images']):
+            image_id = image['id']
+
+            try:
+                image_file_name = image['file_name']
+                image_width = 1280
+                image_height = 720
+                image_path = os.path.join(images_dir, image_file_name)
+                image_annotations = annotations[image_id]
+            except KeyError as key:
+                print("Warning: Skipping image (id: {}) with missing key: {}".format(image_id, key))
+
+            # Add the image using the base method from utils.Dataset
+            self.add_image(
+                source=source_name,
+                image_id=image_id,
+                path=image_path,
+                width=image_width,
+                height=image_height,
+                annotations=image_annotations
+            )
+
+    def load_mask(self, image_id):
+        """ Load instance masks for the given image.
+        MaskRCNN expects masks in the form of a bitmap [height, width, instances].
+        Args:
+            image_id: The id of the image to load masks for
+        Returns:
+            masks: A bool array of shape [height, width, instance count] with
+                one mask per instance.
+            class_ids: a 1D array of class IDs of the instance masks.
+        """
+        image_info = self.image_info[image_id]
+        annotations = image_info['annotations']
+        instance_masks = []
+        class_ids = []
+        
+        for annotation in annotations:
+            class_id = annotation['category_id']
+            mask = Image.new('1', (image_info['width'], image_info['height']))
+            mask_draw = ImageDraw.ImageDraw(mask, '1')
+            for segmentation in annotation['segmentation']:
+                try:
+                    mask_draw.polygon(segmentation, fill=1)
+                    bool_array = np.array(mask) > 0
+                    instance_masks.append(bool_array)
+                    class_ids.append(class_id)
+                except:
+                    continue
+
+
+        mask = np.dstack(instance_masks)
+        class_ids = np.array(class_ids, dtype=np.int32)
+        
+        return mask, class_ids
+
+    def image_reference(self, image_id):
+        """Return the path of the image."""
+        info = self.image_info[image_id]
+        if info["source"] == "bdd100k":
+            return info["id"]
+        else:
+            super(self.__class__, self).image_reference(image_id)
+
+
+############################################################
+#  Training
+############################################################
+
+def train(model, dataset_dir, subset):
+    """Train the model."""
+    # Training dataset.
+    dataset_train = Bdd100kDataset()
+    dataset_train.load_bdd100k(dataset_dir, 'train')
+    dataset_train.prepare()
+
+    # Validation dataset
+    dataset_val = Bdd100kDataset()
+    dataset_val.load_bdd100k(dataset_dir, "val")
+    dataset_val.prepare()
+
+    # Image augmentation
+    # http://imgaug.readthedocs.io/en/latest/source/augmenters.html
+    augmentation = iaa.SomeOf((0, 2), [
+        iaa.Fliplr(0.5),
+        iaa.Flipud(0.5),
+        iaa.OneOf([iaa.Affine(rotate=90),
+                   iaa.Affine(rotate=180),
+                   iaa.Affine(rotate=270)]),
+        iaa.Multiply((0.8, 1.5)),
+        iaa.GaussianBlur(sigma=(0.0, 5.0))
+    ])
+
+    # *** This training schedule is an example. Update to your needs ***
+
+    # If starting from imagenet, train heads only for a bit
+    # since they have random weights
+    print("Train network heads")
+    model.train(dataset_train, dataset_val,
+                learning_rate=config.LEARNING_RATE,
+                epochs=20,
+                augmentation=augmentation,
+                layers='heads')
+
+    print("Train all layers")
+    model.train(dataset_train, dataset_val,
+                learning_rate=config.LEARNING_RATE,
+                epochs=40,
+                augmentation=augmentation,
+                layers='all')
+
+
+############################################################
+#  RLE Encoding
+############################################################
+
+def rle_encode(mask):
+    """Encodes a mask in Run Length Encoding (RLE).
+    Returns a string of space-separated values.
+    """
+    assert mask.ndim == 2, "Mask must be of shape [Height, Width]"
+    # Flatten it column wise
+    m = mask.T.flatten()
+    # Compute gradient. Equals 1 or -1 at transition points
+    g = np.diff(np.concatenate([[0], m, [0]]), n=1)
+    # 1-based indicies of transition points (where gradient != 0)
+    rle = np.where(g != 0)[0].reshape([-1, 2]) + 1
+    # Convert second index in each pair to lenth
+    rle[:, 1] = rle[:, 1] - rle[:, 0]
+    return " ".join(map(str, rle.flatten()))
+
+
+def rle_decode(rle, shape):
+    """Decodes an RLE encoded list of space separated
+    numbers and returns a binary mask."""
+    rle = list(map(int, rle.split()))
+    rle = np.array(rle, dtype=np.int32).reshape([-1, 2])
+    rle[:, 1] += rle[:, 0]
+    rle -= 1
+    mask = np.zeros([shape[0] * shape[1]], np.bool)
+    for s, e in rle:
+        assert 0 <= s < mask.shape[0]
+        assert 1 <= e <= mask.shape[0], "shape: {}  s {}  e {}".format(shape, s, e)
+        mask[s:e] = 1
+    # Reshape and transpose
+    mask = mask.reshape([shape[1], shape[0]]).T
+    return mask
+
+
+def mask_to_rle(image_id, mask, scores):
+    "Encodes instance masks to submission format."
+    assert mask.ndim == 3, "Mask must be [H, W, count]"
+    # If mask is empty, return line with image ID only
+    if mask.shape[-1] == 0:
+        return "{},".format(image_id)
+    # Remove mask overlaps
+    # Multiply each instance mask by its score order
+    # then take the maximum across the last dimension
+    order = np.argsort(scores)[::-1] + 1  # 1-based descending
+    mask = np.max(mask * np.reshape(order, [1, 1, -1]), -1)
+    # Loop over instance masks
+    lines = []
+    for o in order:
+        m = np.where(mask == o, 1, 0)
+        # Skip if empty
+        if m.sum() == 0.0:
+            continue
+        rle = rle_encode(m)
+        lines.append("{}, {}".format(image_id, rle))
+    return "\n".join(lines)
+
+
+############################################################
+#  Detection
+############################################################
+
+def detect(model, dataset_dir, subset):
+    """Run detection on images in the given directory."""
+    print("Running on {}".format(dataset_dir))
+
+    # Create directory
+    if not os.path.exists(RESULTS_DIR):
+        os.makedirs(RESULTS_DIR)
+    submit_dir = "submit_{:%Y%m%dT%H%M%S}".format(datetime.datetime.now())
+    submit_dir = os.path.join(RESULTS_DIR, submit_dir)
+    os.makedirs(submit_dir)
+
+    # Read dataset
+    dataset = Bdd100ksDataset()
+    dataset.load_bdd100k(dataset_dir, subset)
+    dataset.prepare()
+    # Load over images
+    submission = []
+    for image_id in dataset.image_ids:
+        # Load image and run detection
+        image = dataset.load_image(image_id)
+        # Detect objects
+        r = model.detect([image], verbose=0)[0]
+        # Encode image to RLE. Returns a string of multiple lines
+        source_id = dataset.image_info[image_id]["id"]
+        rle = mask_to_rle(source_id, r["masks"], r["scores"])
+        submission.append(rle)
+        # Save image with masks
+        visualize.display_instances(
+            image, r['rois'], r['masks'], r['class_ids'],
+            dataset.class_names, r['scores'],
+            show_bbox=False, show_mask=False,
+            title="Predictions")
+        plt.savefig("{}/{}.png".format(submit_dir, dataset.image_info[image_id]["id"]))
+
+    # Save to csv file
+    submission = "ImageId,EncodedPixels\n" + "\n".join(submission)
+    file_path = os.path.join(submit_dir, "submit.csv")
+    with open(file_path, "w") as f:
+        f.write(submission)
+    print("Saved to ", submit_dir)
+
+
+############################################################
+#  Command Line
+############################################################
+
+if __name__ == '__main__':
+    import argparse
+
+    # Parse command line arguments
+    parser = argparse.ArgumentParser(
+        description='Mask R-CNN for bdd100k counting and segmentation')
+    parser.add_argument("command",
+                        metavar="<command>",
+                        help="'train' or 'detect'")
+    parser.add_argument('--dataset', required=False,
+                        metavar="/path/to/dataset/",
+                        help='Root directory of the dataset')
+    parser.add_argument('--weights', required=True,
+                        metavar="/path/to/weights.h5",
+                        help="Path to weights .h5 file or 'coco'")
+    parser.add_argument('--logs', required=False,
+                        default=DEFAULT_LOGS_DIR,
+                        metavar="/path/to/logs/",
+                        help='Logs and checkpoints directory (default=logs/)')
+    parser.add_argument('--subset', required=False,
+                        metavar="Dataset sub-directory",
+                        help="Subset of dataset to run prediction on")
+    args = parser.parse_args()
+
+    # Validate arguments
+    if args.command == "train":
+        assert args.dataset, "Argument --dataset is required for training"
+    elif args.command == "detect":
+        assert args.subset, "Provide --subset to run prediction on"
+
+    print("Weights: ", args.weights)
+    print("Dataset: ", args.dataset)
+    if args.subset:
+        print("Subset: ", args.subset)
+    print("Logs: ", args.logs)
+
+    # Configurations
+    if args.command == "train":
+        config = Bdd100kConfig()
+    else:
+        config = Bdd100kInferenceConfig()
+    config.display()
+
+    # Create model
+    if args.command == "train":
+        model = modellib.MaskRCNN(mode="training", config=config,
+                                  model_dir=args.logs)
+    else:
+        model = modellib.MaskRCNN(mode="inference", config=config,
+                                  model_dir=args.logs)
+
+    # Select weights file to load
+    if args.weights.lower() == "coco":
+        weights_path = COCO_WEIGHTS_PATH
+        # Download weights file
+        if not os.path.exists(weights_path):
+            utils.download_trained_weights(weights_path)
+    elif args.weights.lower() == "last":
+        # Find last trained weights
+        weights_path = model.find_last()
+    elif args.weights.lower() == "imagenet":
+        # Start from ImageNet trained weights
+        weights_path = model.get_imagenet_weights()
+    else:
+        weights_path = args.weights
+
+    # Load weights
+    print("Loading weights ", weights_path)
+    if args.weights.lower() == "coco":
+        # Exclude the last layers because they require a matching
+        # number of classes
+        model.load_weights(weights_path, by_name=True, exclude=[
+            "mrcnn_class_logits", "mrcnn_bbox_fc",
+            "mrcnn_bbox", "mrcnn_mask"])
+    else:
+        model.load_weights(weights_path, by_name=True)
+
+    # Train or evaluate
+    if args.command == "train":
+        train(model, args.dataset, args.subset)
+    elif args.command == "detect":
+        detect(model, args.dataset, args.subset)
+    else:
+        print("'{}' is not recognized. "
+              "Use 'train' or 'detect'".format(args.command))
